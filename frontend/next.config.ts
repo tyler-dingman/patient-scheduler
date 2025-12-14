be3089a2c107685c31cc01051506b412bb895691
tyler-dingman <tdingman5@gmail.com>
2025-12-13 20:30:04 -0600

    Apply Optum branding to scheduler UI

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
